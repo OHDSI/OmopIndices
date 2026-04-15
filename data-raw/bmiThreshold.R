e08d43af8b5a4ea4380ed62dcdaf6911c3538a5d
catalamarti <marticatalasabate@gmail.com>
2026-04-15 22:43:59 +0100

    remove obsesity cohort

--- data-raw/bmiThreshold.R
+++
@@ -1,7 +0,0 @@
-
-bmiThreshold <- readr::read_csv(
-  file = "data-raw/bmi_threshold.csv",
-  col_types = c(sex = "c", age_min = "d", age_max = "d", bmi_threshold = "d")
-)
-
-usethis::use_data(bmiThreshold, overwrite = TRUE)
