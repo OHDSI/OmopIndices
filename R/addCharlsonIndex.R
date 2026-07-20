85f68efd98dc9124e0d6ed46eb2683576b5b14a1
Marta Alcalde-Herraiz <91142894+martaalcalde@users.noreply.github.com>
2026-07-20 10:53:06 +0100

    Improve cci example

--- R/addCharlsonIndex.R
+++ R/addCharlsonIndex.R
@@ -24,23 +24,23 @@
 #'  mockCohort()
 #'
 #'conceptSet <- list(
-#'  "myocardial_infarction" = 4116491L,
-#'  "congestive_heart_failure" = 4113008L,
-#'  "peripheral_vascular_disease" = 4156265L,
-#'  "cerebrovascular_disease" = 4155034L,
-#'  "dementia" = 4094814L,
-#'  "chronic_pulmonary_disease" = 4048695L,
-#'  "connective_tissue_disease" = 40486433L,
-#'  "peptic_ulcer_disease" = 4051466L,
-#'  "mild_liver_disease" = 4142905L,
-#'  "moderate_or_severe_liver_disease" = 4144583L,
-#'  "diabetes_without_complication" = 4144583L,
-#'  "diabetes_with_complication" = 4144583L,
-#'  "hemiplegia" = 4144583L,
-#'  "severe_chronic_kidney_disease" = 4144583L,
-#'  "any_malignancy" = 4144583L,
-#'  "metastatic_solid_tumor" = 4144583L,
-#'  "aids" = 4144583L)
+#'  "myocardial_infarction" = 329847L,
+#'  "congestive_heart_failure" = 319835L,
+#'  "peripheral_vascular_disease" = 321052L,
+#'  "cerebrovascular_disease" = 381591L,
+#'  "dementia" = 4182210L,
+#'  "chronic_pulmonary_disease" = 255573L,
+#'  "connective_tissue_disease" = 4134537L,
+#'  "peptic_ulcer_disease" = 4027663L,
+#'  "mild_liver_disease" = 194984L,
+#'  "moderate_or_severe_liver_disease" = 4212540L,
+#'  "diabetes_without_complication" = 201820L,
+#'  "diabetes_with_complication" = 42538715L,
+#'  "hemiplegia" = 374022L,
+#'  "severe_chronic_kidney_disease" = 46271022L,
+#'  "any_malignancy" = 4180914L,
+#'  "metastatic_solid_tumor" = 432851L,
+#'  "aids" = 4267414L)
 #'
 #' cdm$cohort |>
 #'   addCharlsonIndex(conceptSet = conceptSet)
