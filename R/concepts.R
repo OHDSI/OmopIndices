aac82b9f76f910cbc4e0314f9fdb33683db251cb
catalamarti <marticatalasabate@gmail.com>
2026-08-05 16:57:02 +0200

    add Charlson concepts

---
+++ R/concepts.R
@@ -0,0 +1,17 @@
+
+#' Indices Concepts
+#'
+#' It contains the default concepts used .
+#'
+#' @format ## `indicesConcepts`
+#' A data frame with 2,792 rows and 7 columns:
+#' \describe{
+#'   \item{index}{Name of the index that uses that concept}
+#'   \item{codelist_name}{Name of the codelist}
+#'   \item{concept_id}{OMOP CDM concept ID}
+#'   \item{concept_name}{OMOP CDM concept name}
+#'   \item{domain_id}{OMOP CDM concept domain}
+#'   \item{vocabulary_id}{Name of the vocabulary ID}
+#'   \item{concept_code}{Original concept code}
+#' }
+"indicesConcepts"
--- data-raw/concepts.R
+++ data-raw/concepts.R
@@ -1,4 +1,44 @@
 
+# read Charlson and Updated Charlson concepts
+charlson <- readr::read_csv(
+  file = here::here("data-raw", "concepts", "charlson.csv"),
+  col_types = c(concept_id = "i", codelist_name = "c", concept_name = "c", vocabulary_id = "c")
+)
 
+# read Hospital Frailty Risk Score
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# create concept dataset
+concepts <- charlson |>
+  dplyr::select("codelist_name", "concept_id") |>
+  dplyr::mutate(index = "charlson") |>
+  dplyr::union_all(
+    charlson |>
+      dplyr::select("codelist_name", "concept_id") |>
+      dplyr::filter(.data$codelist_name %in% .env$updatedCharlsonConcepts) |>
+      dplyr::mutate(index = "updatedCharlson")
+  ) |>
+  dplyr::select("index", "codelist_name", "concept_id") |>
+  dplyr::arrange(.data$index, .data$codelist_name, .data$concept_id)
+
+# add concept name
+cdm <- omock::mockCdmFromDataset(datasetName = "delphi-100k")
+concepts <- concepts |>
+  dplyr::left_join(
+    cdm$concept |>
+      dplyr::select(
+        "concept_id", "concept_name", "domain_id", "vocabulary_id",
+        "concept_code"
+      ),
+    by = "concept_id"
+  )
+
+# check if any name is not present
+x <- concepts |>
+  dplyr::filter(is.na(.data$concept_name))
+if (nrow(x) > 0) {
+  cli::cli_abort(c(x = "There are concepts that are not present in cdm."))
+}
+
+indicesConcepts <- concepts
+
+usethis::use_data(indicesConcepts, overwrite = TRUE)
